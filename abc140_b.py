b6cdb09f6b581bc634c58b411ca0657516de75f0
hsshwtnb <hsshwtnb@gmail.com>
2020-04-02 17:44:56 +0900

    abc140 AC

--- abc140_b.py
+++ abc140_b.py
@@ -4,10 +4,10 @@ b = [int(y) for y in input().split()]
 c = [int(z) for z in input().split()]
 
 total = 0
-pre = 0
-for i in a:
-    total += b[i-1]
-    if i-1 == pre:
-        total += c[pre]
-    pre = i
+pre = 999
+for i in range(N):
+    total += b[a[i]-1]
+    if a[i] == pre+1:
+        total += c[pre-1]
+    pre = a[i]
 print(total)
